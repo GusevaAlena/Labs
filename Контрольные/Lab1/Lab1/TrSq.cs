8d879188e380fc0ec515324ed9183c16693dd4f5
GusevaAlena <73077318+GusevaAlena@users.noreply.github.com>
2020-10-21 00:41:30 +0300

    'ex'

---
+++ Контрольные/Lab1/Lab1/TrSq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class TrSq
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение периметра равностороннего треугольника");
+            double P = double.Parse(Console.ReadLine());
+            double a = P / 3;
+            double p = P / 2;
+            double S = Math.Sqrt(p * Math.Pow((p - a), 3));
+            string st = String.Format("{0:f2}", a);
+            string pl = String.Format("{0:f2}", S);
+            Console.WriteLine("Сторона\t Площадь\n {0}\t {1}", st ,pl);
+            Console.ReadLine();
+        }
+    }
+}
