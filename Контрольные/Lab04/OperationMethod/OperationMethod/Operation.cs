28f1e456d4fe7ed75a7dea82d587092d83065042
GusevaAlena <73077318+GusevaAlena@users.noreply.github.com>
2020-10-25 16:39:49 +0300

    updated 20201025

---
+++ Контрольные/Lab04/OperationMethod/OperationMethod/Operation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationMethod
+{
+    class Operation
+    {
+        public static double TrSqr (double a, double b, double c, double S)
+        {
+            bool ok;
+            ok = Operation.Existance (a, b, c);
+            if (ok==true)
+            {                
+                double p = (a + b + c) / 2;
+                S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return S;
+            }
+           else
+            {
+                Console.WriteLine("Невозможно посчитать площадь");
+                return S;
+            }
+            
+        }
+        private static bool Existance (double a, double b, double c)
+        {          
+           
+            bool Exist;
+            if ((a+b) > c && (b+c) > a && (a+c) > b) 
+            {              
+                Exist = true;
+                Console.WriteLine("Треугольник существует");                                            
+            }
+            else
+            {
+                Exist = false;
+                Console.WriteLine("Треугольника не существует");              
+            }
+            return Exist;
+        }
+        public static double TrSqr(double a,  double S)
+        {                       
+            double p = a * 3 / 2;
+            S = Math.Sqrt(p * Math.Pow((p - a), 3));                         
+            return S;
+        }
+    }
+}
---
+++ Контрольные/Lab04/OperationMethod/OperationMethod/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Равносторонний ли треугольник (введите 'yes', если да, введите 'no', если нет): ");
+            string type = Console.ReadLine();
+            double SqrEqTr=0;
+            double SqrTr = 0;           
+            if (type=="yes")
+            {
+                Console.Write("Введите длину стороны равностороннего треугольника: ");
+                double storona = double.Parse(Console.ReadLine());
+                SqrEqTr = Operation.TrSqr(storona, SqrEqTr);
+                Console.WriteLine("Площадь равностороннего треугольника = " + $"{SqrEqTr:.##}");
+            }
+            else if (type=="no")
+            {
+                Console.Write("Введите длину стороны a: ");
+                double sta = double.Parse(Console.ReadLine());
+                Console.Write("Введите длину стороны b: ");
+                double stb = double.Parse(Console.ReadLine());
+                Console.Write("Введите длину стороны c: ");
+                double stc = double.Parse(Console.ReadLine());
+                SqrTr = Operation.TrSqr(sta, stb, stc, SqrTr);
+                if (SqrTr == 0)
+                {
+                    Console.WriteLine(" ");
+                }
+                else
+                {
+                    Console.WriteLine("Площадь треугольника равна = " + $"{SqrTr:.##}");
+                }
+                
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
