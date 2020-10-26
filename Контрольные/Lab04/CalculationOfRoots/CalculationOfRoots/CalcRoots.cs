183285ae73c9925a008bc74f232839ecb6f518f9
GusevaAlena <73077318+GusevaAlena@users.noreply.github.com>
2020-10-26 20:24:53 +0300

    updated 20201026

---
+++ Контрольные/Lab04/CalculationOfRoots/CalculationOfRoots/CalcRoots.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationOfRoots
+{
+    class CalcRoots
+    {
+        public static int SquareRoots (double a, double b, double c, ref double x1, ref double x2)
+        {           
+            int n=0;
+            double D = Math.Pow(b, 2) - 4 * a * c;
+            if (D>0)
+            {
+                n = 1;
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
+            }
+            else if (D==0)
+            {
+                n = 0;
+                x1 = (-b) / (2 * a);
+            }
+            else if (D<0)
+            {
+                n = -1;
+            }
+            return n;
+        }
+    }
+}
---
+++ Контрольные/Lab04/CalculationOfRoots/CalculationOfRoots/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationOfRoots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = 0;
+            double x2 = 0;
+            int result = 0;
+            Console.Write("Введите коэффициент а: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите коэффициент b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите коэффициент c: ");
+            double c = double.Parse(Console.ReadLine());
+            result = CalcRoots.SquareRoots(a, b, c, ref x1, ref x2);
+            if (result==1)
+            {
+                Console.WriteLine("Корни уравнения с коэффициентами a = {0}, b = {1}, c = {2} равны: x1 = {3}, x2 = {4}", a, b, c, $"{x1:.##}", $"{x2:.##}");
+            }
+            else if (result == 0)
+            {
+                Console.WriteLine("Корень уравнения с коэффициентами a = {0}, b = {1}, c = {2} один: x1 = x2 = {3}", a, b, c, $"{x1:.##}");
+            }
+            else if (result == -1)
+            {
+                Console.WriteLine("Корней уравнения с коэффициентами a = {0}, b = {1}, c = {2} нет", a, b, c);
+            }
+            Console.ReadLine();
+        }
+    }
+}
