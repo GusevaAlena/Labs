81aad7666f34d4676fc52742500f7ac5f68edbf8
GusevaAlena <73077318+GusevaAlena@users.noreply.github.com>
2020-11-01 16:23:57 +0300

    updated 20201101

---
+++ Контрольные/Lab07/BookSort/BookSort/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSort
+{
+    class Book: IComparable
+    {
+        private string author;
+        private string title;
+        private int year;
+        private int pages;
+        public void SetBook(String author, String title, int year, int pages)
+        {
+            this.author = author;
+            this.title = title;
+            this.year = year;
+            this.pages = pages;
+        }
+        public void Show()
+        {
+            Console.WriteLine("\nКнига: \nАвтор: {0}\nНазвание: {1}\nГод издания: {2}\nКоличество страниц:{3}\n", author, title, year, pages);
+        }
+        int IComparable.CompareTo(object obj)
+        {
+            Book it = (Book)obj;
+            if (this.year == it.year) return 0;
+            else if (this.year > it.year) return 1;
+            else return -1;
+        }
+    }
+}
---
+++ Контрольные/Lab07/BookSort/BookSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSort
+{
+    class Program
+    { 
+        public static void ShowList(Book [] books)
+        {
+            for (int i = 0; i < books.Length; i++)
+            {
+                books[i].Show();
+            }
+        }
+        static void Main(string[] args)
+        {          
+            Book b1 = new Book();         
+            b1.SetBook("Пушкин А.С.", "Евгений Онегин", 2017, 384);           
+            Book b2 = new Book();        
+            b2.SetBook("Робин Шарма", "Монах, который продал свой феррари", 2019, 256);          
+            Book b3 = new Book();
+            b3.SetBook("Дэн Браун", "Инферно", 2018, 576);          
+            Book b4 = new Book();          
+            b4.SetBook("Достаевский Ф.М.", "Преступление и наказание", 1996, 544);           
+            Book b5 = new Book();
+            b5.SetBook("Носов Н.Н.", "Приключения Незнайки и его друзей", 2005, 184);
+            Console.WriteLine("До сортировки: ");     
+            Book[] AllBooks = { b1, b2, b3, b4, b5 };
+            ShowList(AllBooks);
+            Array.Sort(AllBooks);
+            Console.WriteLine("После сортировки: ");
+            ShowList(AllBooks);        
+            Console.ReadLine();           
+        }
+    }
+}
