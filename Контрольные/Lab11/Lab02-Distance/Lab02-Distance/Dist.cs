3a689a7328e5157f2a1bd0c0c73828b9d6ca161c
GusevaAlena <73077318+GusevaAlena@users.noreply.github.com>
2020-11-10 01:30:17 +0300

    updated 20201110

--- Контрольные/Lab11/Lab02-Distance/Lab02-Distance/Dist.cs
+++ Контрольные/Lab11/Lab02-Distance/Lab02-Distance/Dist.cs
@@ -9,9 +9,9 @@ namespace Lab02_Distance
     class Distance
     {
         int foot { get; set; }
-        double inch { get; set; }
+        float inch { get; set; }
         public Distance() { }
-        public Distance(int Foot,double Inch)
+        public Distance(int Foot,float Inch)
         {
             foot = Foot;
             inch = Inch;
@@ -21,47 +21,72 @@ namespace Lab02_Distance
         {
             
             int newFoot = d1.foot + d2.foot;
-            double newInch = d1.inch + d2.inch;
+            float newInch = d1.inch + d2.inch;
             return new Distance(newFoot, newInch);
         }
         public static Distance operator - (Distance d1, Distance d2)
         {
             int newFoot = Math.Abs( d1.foot - d2.foot);
-            double newInch = Math.Abs(d1.inch - d2.inch);
+            float newInch = Math.Abs(d1.inch - d2.inch);
             return new Distance(newFoot, newInch);
         }
         public override string ToString()
         {
-            return String.Format("{0}'-{1}''", foot, inch);
-        }
+            return String.Format(foot + "'" + "-" + $"{inch:#####}"+"''");
+        }    
+       
         private void Normalize()
         {                  
-            foot += (inch / 12);
+            foot += (int)inch / 12;
             inch %= 12;
         }
+        private float TotalInches()
+        {
+            return foot * 12 + inch;
+        }
+        public static bool operator >(Distance d1, Distance d2)
+        {
+            return (d1.TotalInches())>(d2.TotalInches());
+        }
+
+        public static bool operator <(Distance d1, Distance d2)
+        {
+            return !((d1 > d2) || (d1 == d2));
+        }
+
     }
     class Dist
     {
         static void Main(string[] args)
         {
-            int Foot = 0;
-            int Inch = 0;
-            Distance d1 = new Distance(Foot, Inch);
+            int Foot;
+            float Inch;
+           
             Console.Write("d1.foot: ");
             Foot= int.Parse(Console.ReadLine());
             Console.Write("d1.inch: ");
-            Inch = int.Parse(Console.ReadLine());
-            Distance d2 = new Distance(Foot, Inch);
+            Inch = float.Parse(Console.ReadLine());
+            Distance d1 = new Distance(Foot, Inch);           
             Console.Write("d2.foot: ");
             Foot = int.Parse(Console.ReadLine());
             Console.Write("d2.inch: ");
-            Inch = int.Parse(Console.ReadLine());
+            Inch = float.Parse(Console.ReadLine());
+            Distance d2 = new Distance(Foot, Inch);
             Distance d3 = new Distance();
             d3 = d1 + d2;
             Console.WriteLine(d3.ToString());
             Distance d4 = new Distance();
             d4 = d1 - d2;
             Console.WriteLine(d4.ToString());
+            if (d1>d2)
+            {
+                Console.WriteLine("{0} больше чем {1}", d1, d2);
+            }
+            else if (d2>d1)
+            {
+                Console.WriteLine("{0} меньше чем {1}", d1, d2);
+            }
+            Console.ReadLine();
         }
     }
 }
