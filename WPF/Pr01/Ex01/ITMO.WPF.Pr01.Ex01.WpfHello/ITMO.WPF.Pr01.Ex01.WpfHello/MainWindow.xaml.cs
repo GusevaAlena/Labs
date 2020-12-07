26fe68c5bfd4b90299d09f1eb1904110ec679629
GusevaAlena <73077318+GusevaAlena@users.noreply.github.com>
2020-12-08 00:37:54 +0300

    updated 20201208

---
+++ WPF/Pr01/Ex01/ITMO.WPF.Pr01.Ex01.WpfHello/ITMO.WPF.Pr01.Ex01.WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace ITMO.WPF.Pr01.Ex01.WpfHello
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Set_Name_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("S:\\username.txt");
+                sw.WriteLine(inputTextBox.Text);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Ret_Name_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("S:\\username.txt");
+                outputLabel.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
