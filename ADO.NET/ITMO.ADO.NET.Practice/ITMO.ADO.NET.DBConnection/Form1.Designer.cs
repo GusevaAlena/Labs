91fa70a7d54ea3343b57abd92103c545c89b72ba
GusevaAlena <73077318+GusevaAlena@users.noreply.github.com>
2021-02-09 23:07:24 +0300

    updated 20210209

---
+++ ADO.NET/ITMO.ADO.NET.Practice/ITMO.ADO.NET.DBConnection/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace ITMO.ADO.NET.DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.работаСБазойДанныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.подключитьсяКБДToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отключитьсяОтБДToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.аToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.списокПодключенийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.работаСБазойДанныхToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // работаСБазойДанныхToolStripMenuItem
+            // 
+            this.работаСБазойДанныхToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.подключитьсяКБДToolStripMenuItem,
+            this.отключитьсяОтБДToolStripMenuItem,
+            this.аToolStripMenuItem,
+            this.списокПодключенийToolStripMenuItem});
+            this.работаСБазойДанныхToolStripMenuItem.Name = "работаСБазойДанныхToolStripMenuItem";
+            this.работаСБазойДанныхToolStripMenuItem.Size = new System.Drawing.Size(215, 29);
+            this.работаСБазойДанныхToolStripMenuItem.Text = "Работа с базой данных";
+            // 
+            // подключитьсяКБДToolStripMenuItem
+            // 
+            this.подключитьсяКБДToolStripMenuItem.Name = "подключитьсяКБДToolStripMenuItem";
+            this.подключитьсяКБДToolStripMenuItem.Size = new System.Drawing.Size(382, 34);
+            this.подключитьсяКБДToolStripMenuItem.Text = "Подключиться к БД";
+            this.подключитьсяКБДToolStripMenuItem.Click += new System.EventHandler(this.подключитьсяКБДToolStripMenuItem_Click);
+            // 
+            // отключитьсяОтБДToolStripMenuItem
+            // 
+            this.отключитьсяОтБДToolStripMenuItem.Name = "отключитьсяОтБДToolStripMenuItem";
+            this.отключитьсяОтБДToolStripMenuItem.Size = new System.Drawing.Size(382, 34);
+            this.отключитьсяОтБДToolStripMenuItem.Text = "Отключиться от БД";
+            this.отключитьсяОтБДToolStripMenuItem.Click += new System.EventHandler(this.отключитьсяОтБДToolStripMenuItem_Click);
+            // 
+            // аToolStripMenuItem
+            // 
+            this.аToolStripMenuItem.Name = "аToolStripMenuItem";
+            this.аToolStripMenuItem.Size = new System.Drawing.Size(382, 34);
+            this.аToolStripMenuItem.Text = "Асинхронное подключение к БД";
+            this.аToolStripMenuItem.Click += new System.EventHandler(this.аToolStripMenuItem_Click);
+            // 
+            // списокПодключенийToolStripMenuItem
+            // 
+            this.списокПодключенийToolStripMenuItem.Name = "списокПодключенийToolStripMenuItem";
+            this.списокПодключенийToolStripMenuItem.Size = new System.Drawing.Size(382, 34);
+            this.списокПодключенийToolStripMenuItem.Text = "Список подключений";
+            this.списокПодключенийToolStripMenuItem.Click += new System.EventHandler(this.списокПодключенийToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сколько продуктов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 68);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Расчет количества продуктов в методе";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(385, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 68);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Список продуктов";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 185);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 253);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название продукта";
+            this.columnHeader1.Width = 180;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Цена";
+            this.columnHeader2.Width = 82;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Фасовка";
+            this.columnHeader3.Width = 119;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(581, 92);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(189, 68);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Транзакция";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem работаСБазойДанныхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem подключитьсяКБДToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отключитьсяОтБДToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem аToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem списокПодключенийToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
