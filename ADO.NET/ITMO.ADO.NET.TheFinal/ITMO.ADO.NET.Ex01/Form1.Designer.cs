ce791826581d92dc95f9cdd85f072005cf782ec1
GusevaAlena <73077318+GusevaAlena@users.noreply.github.com>
2021-02-19 16:09:58 +0300

    updated 21200219

---
+++ ADO.NET/ITMO.ADO.NET.TheFinal/ITMO.ADO.NET.Ex01/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ITMO.ADO.NET.Ex01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.sqlCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.QueryButton = new System.Windows.Forms.Button();
+            this.DateStartPicker = new System.Windows.Forms.DateTimePicker();
+            this.DateEndPicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=INFINITY\\SQLEXPRESS;Initial Catalog=AdventureWorks2017;Integrated Sec" +
+    "urity=True";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // sqlCommand1
+            // 
+            this.sqlCommand1.CommandText = "SELECT ProductID, LineTotal, ModifiedDate, SalesOrderID, SalesOrderDetailID, Orde" +
+    "rQty\r\nFROM   Sales.SalesOrderDetail\r\nWHERE (ModifiedDate BETWEEN @DateStart AND " +
+    "@DateEnd)";
+            this.sqlCommand1.Connection = this.sqlConnection1;
+            this.sqlCommand1.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@DateStart", System.Data.SqlDbType.DateTime, 8, "ModifiedDate"),
+            new System.Data.SqlClient.SqlParameter("@DateEnd", System.Data.SqlDbType.DateTime, 8, "ModifiedDate")});
+            // 
+            // QueryButton
+            // 
+            this.QueryButton.Location = new System.Drawing.Point(592, 255);
+            this.QueryButton.Name = "QueryButton";
+            this.QueryButton.Size = new System.Drawing.Size(144, 37);
+            this.QueryButton.TabIndex = 1;
+            this.QueryButton.Text = "Запрос данных";
+            this.QueryButton.UseVisualStyleBackColor = true;
+            this.QueryButton.Click += new System.EventHandler(this.QueryButton_Click);
+            // 
+            // DateStartPicker
+            // 
+            this.DateStartPicker.Location = new System.Drawing.Point(592, 137);
+            this.DateStartPicker.MinDate = new System.DateTime(2010, 12, 31, 0, 0, 0, 0);
+            this.DateStartPicker.Name = "DateStartPicker";
+            this.DateStartPicker.Size = new System.Drawing.Size(175, 26);
+            this.DateStartPicker.TabIndex = 3;
+            // 
+            // DateEndPicker
+            // 
+            this.DateEndPicker.Location = new System.Drawing.Point(592, 186);
+            this.DateEndPicker.MinDate = new System.DateTime(2010, 12, 31, 0, 0, 0, 0);
+            this.DateEndPicker.Name = "DateEndPicker";
+            this.DateEndPicker.Size = new System.Drawing.Size(175, 26);
+            this.DateEndPicker.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(588, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Период продаж";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(553, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "С";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(553, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ПО";
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ResultTextBox.Multiline = true;
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ResultTextBox.Size = new System.Drawing.Size(514, 426);
+            this.ResultTextBox.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DateEndPicker);
+            this.Controls.Add(this.DateStartPicker);
+            this.Controls.Add(this.QueryButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Data.SqlClient.SqlCommand sqlCommand1;
+        private System.Windows.Forms.Button QueryButton;
+        private System.Windows.Forms.DateTimePicker DateStartPicker;
+        private System.Windows.Forms.DateTimePicker DateEndPicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ResultTextBox;
+    }
+}
+
